23587dbddb49ec1ff0450943e144c8744a354bec
Adam Louis <adamlouis@gmail.com>
2016-06-09 21:53:55 -0700

    add (mandatory) username to user

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -1,11 +1,23 @@
 require 'rails_helper'
 
 feature "signing up" do
-  let(:user) { User.new(email: 'test@example.com', password: '12345678') }
+  let(:user) { User.new(username: 'testuser', email: 'test@example.com', password: '12345678') }
 
-  scenario "with email is successful" do
+  scenario "with email alone is unsuccessful" do
     visit('/')
     click_link('Sign Up')
+    fill_in('Email', with: user.email)
+    fill_in('Password', with: user.password)
+    fill_in('Password confirmation', with: user.password)
+    click_button('Sign up')
+    expect(page).to_not have_content('signed up successfully')
+    expect(page).to have_content('error')
+  end
+
+  scenario "with username and email is successful" do
+    visit('/')
+    click_link('Sign Up')
+    fill_in('Username', with: user.username)
     fill_in('Email', with: user.email)
     fill_in('Password', with: user.password)
     fill_in('Password confirmation', with: user.password)
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.new(username: 'testuser', email: 'test@example.com', password: '12345678') }
+
+  describe 'username attribute' do
+    it 'can be valid' do
+      expect(user).to be_valid
+    end
+
+    it 'is invalid when blank' do
+      user.username = ''
+      expect(user).to_not be_valid
+    end
+
+    it 'is invalid when not unique' do
+      user.save!
+      other_user = User.new(email: 'test2@example.com', password: user.password, username: user.username)
+      expect(other_user).to_not be_valid
+    end
+
+    # Disallowing whitespace is a design decision, up to you. Can be more or less restrictive.
+    it 'is invalid when it contains whitespace' do
+      user.username = 'test user'
+      expect(user).to_not be_valid
+    end
+  end
 end
