3250ec656c5b65a35721d2e0ad327d107b4ff956
Adam Louis <adamlouis@gmail.com>
2016-06-09 20:43:57 -0700

    implement devise default sign up/in/out

---
+++ spec/features/sign_in_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+feature "signing in" do
+  let(:user) { User.create!(email: 'test@example.com', password: '12345678') }
+
+  scenario "with email is successful" do
+    visit('/')
+    click_link('Sign In')
+    fill_in('Email', with: user.email)
+    fill_in('Password', with: user.password)
+    click_button('Log in')
+    expect(page).to have_content('Signed in successfully')
+  end
+end
