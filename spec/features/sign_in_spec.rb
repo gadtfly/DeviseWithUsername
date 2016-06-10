760e6c15fe11d324256810e8c44a77775b0dd880
Adam Louis <adamlouis@gmail.com>
2016-06-09 22:29:44 -0700

    allow sign in by username only
    Note: at this point, you could remove `email` entirely, if you wanted.

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 feature "signing in" do
   let(:user) { User.create!(username: 'testuser', email: 'test@example.com', password: '12345678') }
 
-  scenario "with email is successful" do
+  scenario "with username is successful" do
     visit('/')
     click_link('Sign In')
-    fill_in('Email', with: user.email)
+    fill_in('Username', with: user.username)
     fill_in('Password', with: user.password)
     click_button('Log in')
     expect(page).to have_content('Signed in successfully')
